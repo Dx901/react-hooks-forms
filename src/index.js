c10e176185c667acc3931b0a47d699cdba2c09ce
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-06 21:23:07 +0300

    Code Along

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+//import Form from './components/Form'
+import ParentComponenet from './components/parentComponent';
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponenet />,
   document.getElementById('root')
 );
